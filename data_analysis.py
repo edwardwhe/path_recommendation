e8d07f0687edb577aad12e2b73cd37cbb6d2a079
edwardwhe <ewhhh@outlook.com>
2025-01-08 07:40:24 +0800

    cleaned assessment submission and use that to get the distribution of student

--- data_analysis.py
+++ data_analysis.py
@@ -2,6 +2,8 @@ from ivan_util import util
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
 import numpy as np
+import seaborn as sns
+import pandas as pd
 
 class DataAnalysis:
     def performance_analysis(self):
@@ -142,8 +144,165 @@ class DataAnalysis:
 
       # print(reloaded_data_dict)
       # print(user_data)
+      
+    def get_time_score(self):
+      user = util.load_json(util, "data/users.json")
+      # get the time spent vs score for each question completed by the user
+      time_spent_vs_score = []
+      for i in range(len(user)):
+        if not user[i].get("assessments"):
+          continue
+        for assessment in user[i]["assessments"]:
+          time_spent_vs_score.append([assessment["duration"]/1000, assessment["score"]])
+          
+      # sort the time spent and scores
+      time_spent_vs_score.sort(key=lambda x: x[0])
+      df = pd.DataFrame(time_spent_vs_score, columns=["time_spent", "score"])
+      return df
+    
+    def find_time_threshold(self):
+      df = self.get_time_score()
+      time_interval = [2, 3]
+      step = 0.5
+      result = []
+      while time_interval[1] <= 93.5:
+        interval_score = df[(df["time_spent"] >= time_interval[0]) & (df["time_spent"] < time_interval[1])]["score"]
+        result.append([time_interval[1], interval_score.mean()])
+        time_interval[1] += step
+      # plot the result
+      result = np.array(result)
+      plt.plot(result[:, 0], result[:, 1])
+      plt.xlabel("Time Spent")
+      plt.ylabel("Score")
+      plt.title("Score vs Time Spent")
+      plt.show()
+      
+      quantiles = np.arange(0.05, 0.5, 0.01)
+      quantile_values = [df["time_spent"].quantile(q) for q in quantiles]
+      plt.figure(figsize=(12, 6))
+      plt.subplot(1, 2, 1)
+      plt.plot(quantiles, quantile_values, marker='o')
+      plt.xlabel("Quantile")
+      plt.ylabel("Time Spent")
+      plt.title("Time Spent Lower Quantile")
+      
+      upper_quantile = 0.95
+      upper_quantile_value = df["time_spent"].quantile(upper_quantile)
+      print(upper_quantile_value)
+      
+      # plot the score for each quantile
+      scores = [df[(df["time_spent"] >= q_val) & (df["time_spent"] <= upper_quantile_value)]["score"].mean() for q_val in quantile_values]
+      plt.subplot(1, 2, 2)
+      plt.plot(quantile_values, scores, marker='o')
+      plt.xlabel("Time Spent")
+      plt.ylabel("Score")
+      plt.title("Score vs Time Spent Lower Quantile")
 
+      plt.tight_layout()
+      plt.show()
+      
+    def get_valid_time_score(self):
+      df = self.get_time_score()
+      lower_quantile_value = 2
+      upper_quantile_value = 93.5
+      return df[(df["time_spent"] <= upper_quantile_value) & (df["time_spent"] >= lower_quantile_value)]
+    
+    def time_score_kde_analysis(self):
+      df = self.get_valid_time_score()
+      print(df["time_spent"].mean(), df["time_spent"].std())
+      sns.kdeplot(df["time_spent"], cmap="Blues", fill=True)
+      plt.xlabel("Time Spent")
+      plt.title("Distribution for Time Spent")
+      plt.show()
+    
+    def get_valid_user_sequence(self):
+      users = util.load_json(util, "data/users.json")
+      valid_users = []
+      for user in users:
+        if not user.get("assessments"):
+          continue
+        valid_assessments = []
+        for assessment in user["assessments"]:
+          if assessment.get("duration") < 2000 or assessment.get("duration") > 93500:
+            continue
+          valid_assessments.append(assessment)
+        if valid_assessments:
+          user["assessments"] = valid_assessments
+          valid_users.append(user)
+      return valid_users
+    
+    def get_valid_user_assessment_sequence(self):
+      users = util.load_json(util, "data/users.json")
+      valid_users = []
+      for user in users:
+        if not user.get("assessments"):
+          continue
+        valid_assessments = []
+        for assessment in user["assessments"]:
+          if assessment.get("duration") < 2000 or assessment.get("duration") > 93500:
+            continue
+          elif assessment.get("assessment_type") == 1 or assessment.get("assessment_type") == 8 or assessment.get("assessment_type") == 9:
+            continue
+          valid_assessments.append(assessment)
+        if valid_assessments:
+          user["assessments"] = valid_assessments
+          valid_users.append(user)
+      return valid_users
+    
+    def get_valid_user_exam_sequence(self):
+      users = util.load_json(util, "data/users.json")
+      valid_users = []
+      for user in users:
+        if not user.get("assessments"):
+          continue
+        valid_assessments = []
+        for assessment in user["assessments"]:
+          if assessment.get("duration") < 2000 or assessment.get("duration") > 93500:
+            continue
+          elif assessment.get("assessment_type") == 5 or assessment.get("assessment_type") == 7:
+            continue
+          valid_assessments.append(assessment)
+        if valid_assessments:
+          user["assessments"] = valid_assessments
+          valid_users.append(user)
+      return valid_users
+    
+    def get_user_level_sequence(self, users):
+      sequence = []
+      question_info = util.load_json(util, "data/assessment_question_info.json")
+      difficulty_mapping = {1: 1, 2: 1.2, 3: 2}
+      for user in users:
+        # sum the score of each assessment
+        total_score = 0
+        for assessment in user["assessments"]:
+          total_score += assessment["score"] * difficulty_mapping[question_info[assessment["question"]]["difficulty"]]
+        sequence.append([user["user_id"], total_score / len(user["assessments"])])
+      
+      return pd.DataFrame(sequence, columns=["user_id", "level"])
+
+    def user_level_analysis(self, unit=-1):
+      users = self.get_valid_user_exam_sequence()
+      valid_users = []
+      for user in users:
+        current_assessments = []
+        for assessment in user["assessments"]:
+          if not assessment:
+            continue
+          elif unit != -1 and unit not in assessment["unit"]:
+            continue
+          current_assessments.append(assessment)
+        if current_assessments and len(current_assessments) > 20:
+          user["assessments"] = current_assessments
+          valid_users.append(user)
+      sequence = self.get_user_level_sequence(valid_users)
+      sns.kdeplot(sequence["level"], cmap="Blues", fill=True)
+      plt.xlabel("Level")
+      plt.show()
+      print(sequence["level"].mean(), sequence["level"].std())
+      print(sequence["level"].quantile(0.25), sequence["level"].quantile(0.75))
+      print(sequence["level"].min(), sequence["level"].max())
+      print(len(sequence))
 
 if __name__ == "__main__":
     data_analysis = DataAnalysis()
-    data_analysis.behavior_analysis()
+    data_analysis.user_level_analysis(3)
